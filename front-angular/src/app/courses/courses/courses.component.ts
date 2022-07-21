320f9651dd131e4e30726ff812c02c884a5522b7
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-07-21 14:19:13 -0300

    Creation of the table and the interface of what will be displayed

--- front-angular/src/app/courses/courses/courses.component.ts
+++ front-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from './../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  // Essa variável é um array do tipo Cursos, definido na interface ./../model/course.ts
+  courses: Course[] = [
+    { _id: "1", name: 'Angular', category: 'front-end'},
+    { _id: "1", name: 'Angular', category: 'front-end'},
+    { _id: "1", name: 'Angular', category: 'front-end'},
+  ];
+
+  // Array de string com o nome das propriedades da interface
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // this.courses = [];
+  }
 
   ngOnInit(): void {
   }
