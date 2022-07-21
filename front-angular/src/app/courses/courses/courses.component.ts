897d02b96baee18ee851f52ee30afea6d5cba5d7
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-07-21 14:33:34 -0300

    Added a card and a toolbar to courses component and created a shared folder to store all the imports in an organized way

--- front-angular/src/app/courses/courses/courses.component.ts
+++ front-angular/src/app/courses/courses/courses.component.ts
@@ -12,8 +12,6 @@ export class CoursesComponent implements OnInit {
   // Essa variável é um array do tipo Cursos, definido na interface ./../model/course.ts
   courses: Course[] = [
     { _id: "1", name: 'Angular', category: 'front-end'},
-    { _id: "1", name: 'Angular', category: 'front-end'},
-    { _id: "1", name: 'Angular', category: 'front-end'},
   ];
 
   // Array de string com o nome das propriedades da interface
