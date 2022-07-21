56b2b352d35467ba164b1e698da6d5ee2836c91e
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-07-21 13:27:34 -0300

    Creation of a module with some routing configurations

---
+++ front-angular/src/app/courses/courses-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CoursesComponent } from './courses/courses.component';
+
+const routes: Routes = [
+  // Quando for apenas /courses/, ou seja, quando vier '' (nada) depois da url definida no router principal,
+  // o CoursesComponente vai ser mostrado na tela
+  { path: '', component: CoursesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CoursesRoutingModule { }
