51e8308a4cb9d1474b2fe73f12d1952a5e399b03
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-07-27 10:35:28 -0300

    Files upload

---
+++ back-spring/src/test/java/com/project/backspring/BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
