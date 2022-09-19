664bc7b1b5aeadad3248d6b77253287781d6318b
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-09-19 11:56:56 -0300

    Http get + Rxjs

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,17 +1,22 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '../../../assets/cursos.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end'}
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
