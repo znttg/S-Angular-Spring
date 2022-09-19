8955f9694b80370cc08b07bb8111945486678eb0
Giovanni Pires Zanatta <giovanni@centrus.org.br>
2022-09-19 10:42:31 -0300

    Material table CSS and creation of a shared module to store all imports from angular material

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { MatTableModule } from '@angular/material/table';
+
+
 
 
 @NgModule({
@@ -13,7 +15,7 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
