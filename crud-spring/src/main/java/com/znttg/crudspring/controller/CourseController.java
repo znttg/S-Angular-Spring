5c6d0275bf0d82d941184a0fe13ae46f483119c2
znttg <zanattagiovanni@gmail.com>
2022-10-04 14:39:06 -0300

    Spring HTTP PUT

--- crud-spring/src/main/java/com/znttg/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/znttg/crudspring/controller/CourseController.java
@@ -24,7 +24,7 @@ public class CourseController {
     @GetMapping("/{id}")
     public ResponseEntity<Course> finById(@PathVariable Long id) {
         return courseRepository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
@@ -37,6 +37,18 @@ public class CourseController {
         return courseRepository.save(course);
         //return ResponseEntity.status(HttpStatus.CREATED)
         //.body(courseRepository.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
